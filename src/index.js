0b6a70ea3cdece2a607333eebc7026c171f5a1ae
SoniaSayago <75644975+SoniaSayago@users.noreply.github.com>
2021-01-18 11:24:50 -0500

    avance de formulario, html, css

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import validator from './validator.js';
-
 console.log(validator);
+
+const tarjeta = document.querySelector('#tarjeta'),
+    btnAbrirFormulario = document.querySelector('#btn-abrir-formulario'),
+    formulario = document.querySelector('#formulario-tarjeta');
+    tarjeta.addEventListener('click', () => {
+    tarjeta.classList.toggle('active');
+});
+
+// * Boton de abrir formulario
+    btnAbrirFormulario.addEventListener('click', () => {
+    btnAbrirFormulario.classList.toggle('active');
+    formulario.classList.toggle('active');
+});
