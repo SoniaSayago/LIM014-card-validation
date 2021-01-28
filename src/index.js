e2c4651764025c646923d860eecdc3ecce921208
SoniaSayago <75644975+SoniaSayago@users.noreply.github.com>
2021-01-28 12:24:40 -0500

    cambios finales con Jest test

--- src/index.js
+++ src/index.js
@@ -1,15 +1,147 @@
-import validator from './validator.js';
-console.log(validator);
 
+/* TARJETA DARLE DINAMIC */
 const tarjeta = document.querySelector('#tarjeta'),
-    btnAbrirFormulario = document.querySelector('#btn-abrir-formulario'),
-    formulario = document.querySelector('#formulario-tarjeta');
-    tarjeta.addEventListener('click', () => {
+        btnAbrirFormulario = document.querySelector('#btn-abrir-formulario'),
+        formulario = document.querySelector('#formulario-tarjeta'),
+        numeroTarjeta = document.querySelector('#tarjeta .numero'),
+        logoMarca = document.querySelector('#logo-marca'),
+        firma = document.querySelector('#tarjeta .firma p'),
+        nombreTarjeta = document.querySelector('#tarjeta .nombre'),
+        mesExpiracion = document.querySelector('#tarjeta .mes'),
+        yearExpiracion = document.querySelector('#tarjeta .year'),
+        ccv = document.querySelector('#tarjeta .ccv');
+
+
+//* Volteamos la tarjeta para mostrar el frente.
+const mostrarFrente = () => {
+    if(tarjeta.classList.contains('active')) {
+        tarjeta.classList.remove('active');
+    }
+}
+
+//* ROTACION DE LA TARJETA *//
+tarjeta.addEventListener('click', () => {
     tarjeta.classList.toggle('active');
 });
 
-// * Boton de abrir formulario
-    btnAbrirFormulario.addEventListener('click', () => {
+// * Boton de abrir formulario *//
+btnAbrirFormulario.addEventListener('click', () => {
     btnAbrirFormulario.classList.toggle('active');
     formulario.classList.toggle('active');
+});
+
+//* Select del mes generado dinamicamente.
+for (let i = 1; i<= 12; i++){
+    let opcion = document.createElement('option');
+    opcion.value = i;
+    opcion.innerText = i;
+    formulario.selectMes.appendChild(opcion);
+}
+
+//* select del año generado dinamicamente
+const yearActual = new Date().getFullYear();
+for(let i = yearActual; i <= yearActual + 8; i++){
+    let opcion = document.createElement('option');
+    opcion.value = i;
+    opcion.innerText = i;
+    formulario.selectYear.appendChild(opcion);
+}
+
+//* INPUT nÙMERO DE TARJETA 
+formulario.inputNumero.addEventListener('keyup', (e) => {
+    let valorInput = e.target.value;
+    
+    formulario.inputNumero.value = valorInput
+    // eliminamos espacios en blanco, letras
+    .replace(/\s/g,'')
+    // eliminar las letras
+    .replace(/\D/g,'')
+    // Espacio cada 4 nùmeros con expresiones regulares
+    .replace(/([0-9]{4})/g,'$1 ')
+    //Elimina el ùltimo espaciado
+    .trim();
+
+    numeroTarjeta.textContent = valorInput
+
+    if(valorInput == ''){
+        numeroTarjeta.textContent = '#### #### #### ####';
+   
+        logoMarca.innerHTML = '';
+    }
+
+    if(valorInput[0] == 4){
+        logoMarca.innerHTML = '';
+        const imagen = document.createElement('img');
+        imagen.src = 'img/logos/visa.png';
+        logoMarca.appendChild(imagen);
+    }
+     else if(valorInput[0] == 5){
+        logoMarca.innerHTML = '';
+        const imagen = document.createElement('img');
+        imagen.src = 'img/logos/mastercard.png';
+        logoMarca.appendChild(imagen);
+    }
+
+    // Volteamos la tarjeta para que el UX vea lo que escribe
+    mostrarFrente()
+});
+
+//* INPUT nombre DE TARJETA 
+formulario.inputNombre.addEventListener('keyup', (e) => {
+    let valorInput = e.target.value;
+
+    formulario.inputNombre.value = valorInput
+    .replace(/\d/g,'')
+    nombreTarjeta.textContent = valorInput;
+    firma.textContent = valorInput;
+
+    if(valorInput == ''){
+        nombreTarjeta.textContent = 'Sonia Sayago';
+    }
+    // Volteamos la tarjeta para que el UX vea lo que escribe
+    mostrarFrente()
+});
+
+//* Select mes
+formulario.selectMes.addEventListener('change', (e) => {
+    mesExpiracion.textContent = e.target.value;
+    // Volteamos la tarjeta para que el UX vea lo que escribe
+    mostrarFrente()
+});
+
+//* Select year
+formulario.selectYear.addEventListener('change', (e) => {
+    yearExpiracion.textContent = e.target.value.slice(2);
+    // Volteamos la tarjeta para que el UX vea lo que escribe
+    mostrarFrente()
+});
+
+//* ccv
+formulario.inputCCV.addEventListener('keyup', () => {
+	if(!tarjeta.classList.contains('active')){
+		tarjeta.classList.toggle('active');
+	}
+
+	formulario.inputCCV.value = formulario.inputCCV.value
+	// Eliminar los espacios
+	.replace(/\s/g, '')
+	// Eliminar las letras
+	.replace(/\D/g, '');
+
+	ccv.textContent = formulario.inputCCV.value;
+});
+
+import validator from './validator.js';
+
+//validate
+document.getElementById('validarNumero').addEventListener('click', () => {
+    let creditCardNumber = document.getElementById('inputNumero').value;
+    let answer = document.getElementById('answer')
+    if (validator.isValid(creditCardNumber) == true) {
+        answer.innerHTML = validator.maskify(creditCardNumber) + " Tarjeta válida"
+        answer.style = 'color:green';
+    } else {
+        answer.innerHTML = validator.maskify(creditCardNumber) + " Tarjeta no válida"
+        answer.style = 'color:red';
+    }
 });
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,65 @@
-const validator = {
-  // ...
-};
+// isValid = algoritmo de Luhn
+const isValid = (numberCard) => {
+  // convert into array
+  let numberArray = numberCard.split('').reverse(); //Separe un número de 2 digitos y sume cada digito
+console.log(numberArray);
+  // first loop
+  let i;
+  for(i=0; i < numberArray.length; i++) {   //Tome en cuenta los que son pares desde el cero, 2, ... (las posiciones)
+  
+    if ((i+1) % 2 == 0) { // multiply second digit
+    
+    let multipliedNumber = numberArray[i] * 2; //Si eso pasa dejalo multiplicar *2
+    numberArray[i] = multipliedNumber.toString(); // lo convierte en string
 
-export default validator;
+    if (multipliedNumber > 9) { // si es mayor que 9
+      let newNumber = numberArray[i].split (''); // se va separar los nùmeros con split
+
+      let sum = parseInt(newNumber[0]) + parseInt(newNumber[1]); //Sumar digito x digito desde la posicion 0 a posicion 1
+      numberArray[1] = sum.toString(); // ver el resultado de esa suma
+    }
+  }
+}
+
+// 
+let total = 0;
+let index = 0;
+
+for (index; index < numberArray.length; index++) { // Index comienza desde 0
+  total = total + parseInt(numberArray[index]); // esta recorriendo un array
+}
+
+  console.log(total)
+  if (total % 10 == 0){
+    return true
+  }else{
+    return false
+  }
+}
+
+
+// Ahora maskify: debe mostrar los · y los ùltimos 4 dígitos
+
+const maskify = (numberCard) => {
+  //ocultar los digitos menos los 4 ultimos
+  if(numberCard.length > 4) {
+      let cardNumberArray = numberCard.split('');
+      let newNumberString = '';
+
+      for (let i = 0;i <cardNumberArray.length; i++) {  // Se le va sumar 1 al i ==> ++ se significa aumentarle 1 más
+          if (i < (cardNumberArray.length - 4)) {
+              newNumberString += '#';
+          } else {
+            newNumberString += cardNumberArray[i];// Si no cumple con la condicional entonces ocultar con #
+          }
+      }
+
+      return newNumberString;
+    } else {
+        return numberCard;
+    }
+}
+
+const validator = { isValid, maskify}
+
+export default validator 
